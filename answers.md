1f832affc6e9bd36ea7a3a92341d55ea16cc5e72
Rebqa Yohannes <ryohannes2018@gmail.com>
2026-01-16 04:21:04 +0000

    Add recipe

---
+++ answers.md
@@ -0,0 +1,7 @@
+# 1. The path is: /workspaces/daytwostuff
+# 2. The environment should not be included in the repo.
+# 3. The terminal path is not different.
+# 4. Data Wrangler on the extension menu displays the description of what functions it provides. It also displays a blue check beneath the extension name beside the company Data Wrangler belongs to (Microsoft). This confirms the comapny is verified.  
+# 5. After reviewing the capabilities of Data Wrangler I believe a few of the useful elements are data viewing, cleaning, and preparation. 
+# 6. Plotly Version: 6.5.1
+# 7. We use a requirements.txt to make sure the packages and the versions of said packages are reproducible on different machines.
---
+++ recipe.py
@@ -0,0 +1,20 @@
+#%%
+# 1. go to github 
+# 2. create a new repository 
+# 3. ensure you are in the codespaces tab and click the plus button next to codespaces, this will redirect you to the website version of VS Code 
+# 4. In the website, click the 3 horizontal lines on the top left corner, click this and navigate to the “Open in VS Code Desktop” option
+# 5. Once you are on the desktop version, on the home page there is a “connect to” option 
+# 6. Click connect to and this will open the search bar where there is a “Connect to Codespace” option
+# 7. After clicking this, the searchbar will prompt you to click the codespace you created through Github, click it. 
+# 8. open a terminal within the repository and create a veirtual enviornment by typing “python3 -m venv “name of your preferred virual environment”” 
+# 9. activate virtual environment by typing “source “name of virtual environment here”/bin/activate”
+# 10. create a new file through the terminal command “nano ‘name of file’.py”
+# 11. open the sidebar menu on the left side and click the symbol that contain four squares
+# 12. find the drop down menu subtab called “Codespaces” 
+# 13. There should be a cloud symbol next to the subtab with an arrow, click this 
+# 14. It will bring up the search bar displaying extensions to install
+# 15. install all extensions with “Python” and “Jupyter” and click the “OK” button (blue) this makes the .py file interactive
+# 16. add contents to the .py file
+# 17. now push to your repository by following these steps: first trype into your terminal “git add . “ then “git commit -m ‘personalized message’” lastly “git push origin main” 
+
+# %%
